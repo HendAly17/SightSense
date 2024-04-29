23fa337765c393870aa67abc22cf73aca53ea575
HendAly17 <hendaly1792001@gmail.com>
2024-04-29 21:06:20 +0300

    voice recognition without search

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
 import 'image_upload_page.dart';
 import 'emergency_contacts_page.dart';
 import 'settings_page.dart';
@@ -37,6 +38,13 @@ class OrderTrackingPage extends StatefulWidget {
 
 class _OrderTrackingPageState extends State<OrderTrackingPage> {
   int _selectedIndex = 0;
+  late stt.SpeechToText _speech;
+
+  @override
+  void initState() {
+    super.initState();
+    _speech = stt.SpeechToText();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -82,19 +90,43 @@ class _OrderTrackingPageState extends State<OrderTrackingPage> {
     );
   }
 
-Widget _buildVoiceCommandButton(IconData icon, int index) {
-  return IconButton(
-    onPressed: () {
-      setState(() {
-        _selectedIndex = index;
-      });
-    },
-    icon: Icon(
-      icon,
-      color: _selectedIndex == index ? Colors.green : Colors.black,
-      size: 65,
-    ),
-  );
-}
+  Widget _buildVoiceCommandButton(IconData icon, int index) {
+    return IconButton(
+      onPressed: () {
+        _startListening();
+      },
+      icon: Icon(
+        icon,
+        color: _selectedIndex == index ? Colors.green : Colors.black,
+        size: 65,
+      ),
+    );
+  }
 
+  void _startListening() async {
+    if (!_speech.isListening) {
+      bool available = await _speech.initialize(
+        onStatus: (status) => print('Status: $status'),
+        onError: (error) => print('Error: $error'),
+      );
+      if (available) {
+        _speech.listen(
+          onResult: (result) {
+            setState(() {
+              // Handle voice input result
+              _handleVoiceInput(result.recognizedWords);
+            });
+          },
+        );
+      } else {
+        print('Speech recognition not available');
+      }
+    }
+  }
+
+  void _handleVoiceInput(String input) {
+    // Implement logic to handle voice input
+    print('Voice input: $input');
+    // Use the input for searching and marking on the map
+  }
 }
